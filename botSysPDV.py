2fbccafd23b6641ac781c6c7505a0b2c6b81e805
Wanderson Fontes <41520210+WandersonFontes@users.noreply.github.com>
2020-05-18 16:59:42 -0300

    new
    correção de send_keys em campos específicos.

--- botSysPDV.py
+++ botSysPDV.py
@@ -3,41 +3,38 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-user_cm = (input(str('Por gentileza insira o para realizar o acesso:\n')))
+
+user_cm  = (input(str('Informe o usuário para acessar o Portal da Revenda:\n')))
 senha_cm = (input(str('Digite sua senha:\n')))
-cnpj_cm = (input(str('Agora informe o CNPJ:\n')))
-serie_cm = (input(str('Qual número de serie?\n')))
-
-def login():
-    driver = webdriver.Chrome()
-    driver.get("http://sistemas.casamagalhaes.com.br/home.cm")
-    time.sleep(3)
-    user = driver.find_element_by_id('usuario')
-    user.send_keys(user_cm)
-    time.sleep(3)
-    senha = driver.find_element_by_id('senha')
-    senha.send_keys(senha_cm)
-    time.sleep(3)
-    entrar = driver.find_element_by_id('enviar').click()
-    time.sleep(3)
-
-    #Inserção de dados para geração da chave de acesso
-    
-    suporte = driver.find_element_by_link_text('Suporte').click()
-    time.sleep(2)
-    chave  = driver.find_element_by_link_text('Chave').click()
-    time.sleep(2)
-    renovaChave  = driver.find_element_by_link_text('Renovação de Chave').click()
-    time.sleep(2)
-    cnpj = driver.find_element_by_id('chaveForm:cpfCnpj')
-    cnpj.send_keys(cnpj_cm)
-    time.sleep(2)
-    confirmar = driver.find_element_by_id('chaveForm:j_id321').click()
-    time.sleep(2)
-    serie  = driver.find_element_by_id('chaveForm:j_id360')
-    serie.send_keys()
-
-
-
-
-
+cnpj_cm  = (input(str('Digite o CNPJ:\n')))
+serie_cm = (input(str('Digite o númerode série:\n')))
+prazo_cm = (input(str('Informe o prazo da chave:\n')))
+driver   = webdriver.Chrome()
+driver.get("http://sistemas.casamagalhaes.com.br/home.cm")
+time.sleep(5)
+user     = driver.find_element_by_id('usuario')
+user.send_keys(user_cm)
+time.sleep(3)
+senha    = driver.find_element_by_id('senha')
+senha.send_keys(senha_cm)
+time.sleep(3)
+entrar   = driver.find_element_by_id('enviar').click()
+time.sleep(3)
+suporte = driver.find_element_by_link_text('Suporte').click()
+time.sleep(2)
+chave  = driver.find_element_by_link_text('Chave').click()
+time.sleep(2)
+renovaChave  = driver.find_element_by_link_text('Renovação de Chave').click()
+time.sleep(2)
+cnpj = driver.find_element_by_id('chaveForm:cpfCnpj')
+cnpj.send_keys(cnpj_cm)
+time.sleep(2)
+confirmar = driver.find_element_by_id('chaveForm:j_id321').click()
+time.sleep(2)
+serie  = driver.find_element_by_id('chaveForm:j_id360')
+serie.send_keys(serie_cm)
+comfirmar = driver.find_element_by_id('chaveForm:j_id325').click()
+time.sleep(2)
+prazo = driver.find_element_by_id('chaveForm:prazo')
+prazo.clear()
+prazo.send_keys(prazo_cm)
